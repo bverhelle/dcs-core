78e1ee9e8571dccf9bc0ada5db2af968d525fd0d
Brecht Verhelle <brecht.verhelle@gmail.com>
2018-01-08 13:56:37 +0100

    First Solution commit

---
+++ DCSCoreMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DCSCoreMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
