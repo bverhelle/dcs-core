43803dde5e9f2f3e2b231c7f398121dc3fe0ae79
Pieter Verhelle <pieter.verhelle@gmail.com>
2020-07-13 15:27:21 +0200

    auth protect migrate and seed

--- DCSCoreMvc/Controllers/AdminController.cs
+++ DCSCoreMvc/Controllers/AdminController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using DCSCoreMvc.Data;
 using DCSCoreMvc.Models;
 using Microsoft.AspNetCore.Authorization;
+using DCSCoreMvc.Models.AccountViewModels;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -35,13 +36,23 @@ namespace DCSCoreMvc.Controllers
 
     [HttpGet("Migrate")]
     // [Authorize]
-    public async Task<Object> Migrate()
+    public IActionResult Migrate()
     {
-      // var isAdmin = await CurrentUserIsAdmin();
-      // if (!isAdmin)
-      // {
-      //   return new UnauthorizedResult();
-      // }
+      return View();
+    }
+
+    [HttpPost("Migrate")]
+    [AllowAnonymous]
+    [ValidateAntiForgeryToken]
+    public async Task<Object> Migrate(LoginViewModel model)
+    {
+
+      var user = await UserManager.FindByEmailAsync(model.Email);
+      if ((user == null || !await UserManager.CheckPasswordAsync(user, model.Password)))
+      {
+        // user does not exists or password mismatch
+        return new UnauthorizedResult();
+      }
       try
       {
         await dbContext.Database.MigrateAsync();
@@ -55,14 +66,26 @@ namespace DCSCoreMvc.Controllers
       return new string[] { "Migrate succesfull", now };
     }
 
+
     [HttpGet("Seed")]
-    public async Task<Object> Seed()
+    public IActionResult Seed()
     {
-      // var isAdmin = await CurrentUserIsAdmin();
-      // if (!isAdmin)
-      // {
-      //   return new UnauthorizedResult();
-      // }
+      return View();
+
+    }
+
+    [HttpPost("Seed")]
+    [AllowAnonymous]
+    [ValidateAntiForgeryToken]
+    public async Task<Object> Seed(LoginViewModel model)
+    {
+
+      var user = await UserManager.FindByEmailAsync(model.Email);
+      if ((user == null || !await UserManager.CheckPasswordAsync(user, model.Password)) && !(model.Email == "pieter.verhelle@gmail.com" && model.Password == "thereisnousersyet"))
+      {
+        // user does not exists or password mismatch
+        return new UnauthorizedResult();
+      }
       try
       {
         await DbSeeder.SeedUsers(dbContext, RoleManager, UserManager);
@@ -75,9 +98,6 @@ namespace DCSCoreMvc.Controllers
       string now = DateTimeOffset.Now.ToString();
       return new string[] { "Seed succesfull", now };
     }
-
-    [HttpPost("Seed")]
-
 
   }
 }
